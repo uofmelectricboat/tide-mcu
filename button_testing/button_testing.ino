4ccbe1c81e05c61d6f60529c77e1d92e279f823d
David Anapolsky <danapols@umich.edu>
2024-04-30 14:03:12 -0400

    initial button testing

---
+++ button_testing/button_testing.ino
@@ -0,0 +1,34 @@
+ /*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-button
+ */
+
+#define BUTTON_PIN 21  // GIOP21 pin connected to button
+
+// Variables will change:
+int lastState = LOW;  // the previous state from the input pin
+int currentState;     // the current reading from the input pin
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // initialize the pushbutton pin as an pull-up input
+  // the pull-up input pin will be HIGH when the switch is open and LOW when the switch is closed.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // read the state of the switch/button:
+  currentState = digitalRead(BUTTON_PIN);
+
+  if (lastState == HIGH && currentState == LOW)
+    Serial.println("The button is pressed");
+  else if (lastState == LOW && currentState == HIGH)
+    Serial.println("The button is released");
+
+  // save the the last state
+  lastState = currentState;
+}
