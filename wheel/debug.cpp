2c8c350fefcabbcd770b57c182deb2fc89cd98aa
David Anapolsky <danapols@umich.edu>
2024-05-19 18:53:52 -0400

    time wasted on debug

--- wheel/debug.cpp
+++ wheel/debug.cpp
@@ -1,47 +1,43 @@
 #include "debug.h"
+
 // Debugging printing to only send to Serial when in DEBUG mode
 
 
-// TODO consider passing by reference
-
 bool DEBUG = true;
 
-void debugMode(bool mode) {
+void mode(bool mode) {
     DEBUG = mode;
 }
 
-void debugPrint(const String val) {
+void print(const String val = "") {
   if (DEBUG) {
     Serial.print(val);
   }
 }
 
-void debugPrintln(const String val) {
-  debugPrint(val);
-  debugPrint("\n");
+void println(const String val = "") {
+  print(val);
+  print("\n");
 }
 
-void debugPrint(const uint8_t val) {
+void print(const uint8_t val) {
   if (DEBUG) {
     Serial.print(val);
   }
 }
 
-void debugPrintln(const uint8_t val) {
-  debugPrint(val);
-  debugPrint("\n");
+void println(const uint8_t val) {
+  print(val);
+  print("\n");
 }
 
-void debugPrint(const uint8_t val, const int format) {
+void print(const uint8_t val, const int format) {
   if (DEBUG) {
     Serial.print(val, format);
   }
 }
 
-void debugPrintln(const uint8_t val, const int format) {
-    debugPrint(val, format);
-    debugPrint("\n");
+void println(const uint8_t val, const int format) {
+    print(val, format);
+    print("\n");
 }
-
-
-
