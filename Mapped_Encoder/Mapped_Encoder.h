02560f278408e99a3c5609b7c5a85151f55e1623
David Anapolsky <danapols@umich.edu>
2024-05-22 00:02:57 -0500

    Mapped Encoder Library

---
+++ Mapped_Encoder/Mapped_Encoder.h
@@ -0,0 +1,40 @@
+/*
+  Mapped_Encoder.h - Library for getting input from a potentiometer or encoder and mapping it
+  Created by David Anapolsky, May 21, 2024.
+*/
+
+#ifndef Mapped_Encoder_h
+#define Mapped_Encoder_h
+
+#include "Arduino.h"
+
+#define BITMAX10 1023
+#define BITMAX12 4095
+
+class Mapped_Encoder {
+  private:
+    int pin;
+    int in_min;
+    int in_max;
+    int out_min;
+    int out_max;
+
+    const int analogMax;
+    const String _name;
+    bool passes_zero;
+
+    void set_passes_zero();
+  
+  public:
+    Mapped_Encoder(int pin, int in_min, int in_max, int out_min, int out_max, const int analogMax, const String name);
+
+    float read();
+    uint16_t readRaw();
+    const String & name();
+    void set_min(int analogVal);
+    void set_max(int analogVal);
+
+    float floatMap(float x);
+};
+
+#endif
--- wheel/wheel.ino
+++ wheel/wheel.ino
@@ -2,46 +2,33 @@
 All the quotes are from here: https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
 */
 
-class Pot {
-  private:
-    int pin;
-    int in_min;
-    int in_max;
-    int out_min;
-    int out_max;
-    const String pot_name;
+#include <Arduino_DebugUtils.h> // Logging
 
-    // From https://esp32io.com/tutorials/esp32-potentiometer
-    float floatMap(float x) {
-      return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
+#include <CAN.h> // CAN
+/*
+CAN default pins
+CAN | ESP32
+3V3	| 3V3
+GND	| GND
+CTX	| GPIO_5
+CRX	| GPIO_4
+*/
 
-  public:
-    Pot(int pin, int in_min, int in_max, int out_min, int out_max, const String name) : 
-      pin(pin), in_min(in_min), in_max(in_max), out_min(out_max), pot_name(name) {}
+#include <Mapped_Encoder.h> // Ecoder and throttle
 
-    float read() {
-      floatMap(readRaw());
-    }
+// Debug mode
+// Options in lowest to highest priority:
+// DBG_NONE, DBG_ERROR, DBG_WARNING, DBG_INFO (default), DBG_DEBUG, DBG_VERBOSE
+#define debuggingLevel DBG_DEBUG
 
-    uint16_t readRaw() {
-      return analogRead(pin);
-    }
+// Can bitrate
+#define bitrateCAN 500E3
 
-    const String & name() {
-      return pot_name;
-    }
-
-    // MODIFIES : analog min value
-    void set_min(int analogVal) {
-      in_min = analogVal;
-    }
-
-    // MODIFIES : analog max value
-    void set_max(int analogVal) {
-      in_max = analogVal;
-    }
-};
+// CAN Ids
+#define encoderId  1999
+#define throttleId 1998
+#define buttonsId  2000
+#define LEDsId     2001
 
 /*
 Steering Wheel Buttons
@@ -65,50 +52,20 @@ E-stop (A-BRB) is intentionally not included
 #define LED5 18
 #define LED6 17
 
-// Lists that contain the Steering Wheel buttons and the LED pins
 #define NUM_BUTTONS 5
 #define NUM_LEDS 6
 
+// Lists that contain the Steering Wheel buttons and the LED pins
 int buttons[NUM_BUTTONS] = {CommsPin, TrimUpPin, TrimDownPin, GainUpPin, GainDownPin};
 const String button_names[NUM_BUTTONS] = {"Comms", "Trim Up", "Trim Down", "Gain Up", "Gain Down"};
 
 int LEDs[NUM_LEDS] = {LED1, LED2, LED3, LED4, LED5, LED6};
 
-#include <CAN.h>
+// Steering wheel encoder
+Mapped_Encoder encoder(32,0,4095,0,360, BITMAX12, "Encoder"); // Scale => [0,360]
 
-/*
-CAN default pins
-CAN | ESP32
-3V3	| 3V3
-GND	| GND
-CTX	| GPIO_5
-CRX	| GPIO_4
-*/
-
-// Can bitrate
-#define bitrateCAN 500E3
-
-// CAN Ids
-#define encoderId  1999
-#define throttleId 1998
-#define buttonsId  2000
-#define LEDsId     2001
-
-
-// For logging
-#include <Arduino_DebugUtils.h>
-
-// Debug mode
-// Options in lowest to highest priority:
-// DBG_NONE, DBG_ERROR, DBG_WARNING, DBG_INFO (default), DBG_DEBUG, DBG_VERBOSE
-#define debuggingLevel DBG_DEBUG
-
-
-  // Steering wheel encoder
-  Pot encoder(32,0,4095,0,360, "Encoder"); // pin, analog min & max, out min & max; Scale => [0,360]
-
-  // Throttle Potentiometer
-  Pot throttle(34,0,4095,0,100, "Throttle"); // pin, analog min & max, out min & max; Scale => [0,100]
+// Throttle Potentiometer
+Mapped_Encoder throttle(34,0,4095,0,100, BITMAX12, "Throttle"); // Scale => [0,100]
 
 void setup() {
 
@@ -141,9 +98,9 @@ void setup() {
 
 void loop() {
 
-  encoderSend();
+  potToCAN(encoderId, encoder.read(), encoder.name());
 
-  throttleSend();
+  potToCAN(throttleId, throttle.read(), throttle.name());
 
   buttonsSend();
 
@@ -170,13 +127,6 @@ void potToCAN(int id, int val, const String name) {
 
 }
 
-void encoderSend() {
-  potToCAN(encoderId, encoder.read(), encoder.name());
-}
-
-void throttleSend() {
-  potToCAN(throttleId, throttle.read(), throttle.name());
-}
 
 void buttonsSend() {
   CAN.beginPacket(buttonsId);
@@ -278,7 +228,7 @@ Steps:
 
 */
 
-bool pot_cal(Pot &pot, int numTrials) {
+bool pot_cal(Mapped_Encoder &pot, int numTrials) {
   DEBUG_INFO("%s Calibration:", pot.name());
 
   // Max number of data points to hold for each trial
